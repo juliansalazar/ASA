4eafa574acc1fddb9b5a332aa03bc1c381443bc4
unknown <juliansalazarvelez94@gmail.com>
2024-07-17 16:30:16 -0500

    Pasar config Gmaps al backend2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,7 +17,6 @@ const Home = () => {
             <Banner />
             <AdditionalServices />
             <Whatsapp />
-            <GoogleMaps />
             <GoogleReview />
             <NuestrosClientes />
             <Footer />
