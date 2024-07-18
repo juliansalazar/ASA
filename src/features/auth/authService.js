0ed507cc714f433eb017beee5f6ccfceb7c705d8
unknown <juliansalazarvelez94@gmail.com>
2024-07-17 20:46:51 -0500

    Direccionar path a autocarest.com

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API_URL = 'http://localhost:8080/api/users/'
+const API_URL = 'http://autocarest.com/api/users/'
 
 const register = async (userData) => {
     const response = await axios.post(API_URL + 'register', userData)
