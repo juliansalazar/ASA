5c9800962649fe4563709f939423dc83f91c0972
unknown <juliansalazarvelez94@gmail.com>
2025-03-16 20:46:26 -0500

    Debug

--- src/components/InvoiceList.jsx
+++ src/components/InvoiceList.jsx
@@ -13,7 +13,7 @@ const InvoiceList = () => {
   const [endDate, setEndDate] = useState(''); // Dejamos endDate vacío para traer hasta la fecha actual
   const [clientId, setClientId] = useState(null); // Estado para el clientId dinámico
 
-  const apiKey = import.meta.env.REACT_APP_VITE_CONTIFICO
+  const apiKey = process.env.REACT_APP_VITE_CONTIFICO
 
   // Función para formatear la fecha
   const formatDateForApi = (date) => {
