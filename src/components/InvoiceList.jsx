63df85c2bf1ef33c05153013b3b27bf96b42ef51
unknown <juliansalazarvelez94@gmail.com>
2025-03-18 10:33:19 -0500

    Debug

--- src/components/InvoiceList.jsx
+++ src/components/InvoiceList.jsx
@@ -121,7 +121,7 @@ const InvoiceList = () => {
   // Cargar el clientId al montar el componente y luego fetchInvoices automáticamente
   useEffect(() => {
     fetchClientId();
-  }, [clientId]);
+  }, []);
 
   // Ejecutar fetchInvoices automáticamente cuando cambie clientId
   useEffect(() => {
